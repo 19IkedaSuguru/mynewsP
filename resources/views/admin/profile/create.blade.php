d68958371461c7fbd0f77a5061c01e26c69d288d
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-08-05 02:02:59 +0000

    php19

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta cherset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Mynews</title>
+    </head>
+    <body>
+        <h1>練習１</h1></h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//追記
+Route::group(['prefix' =>'admin'], function() {
+    Route::get('news/create', 'Admin\NewController@add');
+});
+
+///課題４
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
