c347a26cdefb2f4a4c455daa9643e21390d96523
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-08-15 12:37:03 +0000

    修正が必要かも8/15

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ Route::get('/', function () {
 
 //追記
 Route::group(['prefix' =>'admin'], function() {
-    Route::get('news/create', 'Admin\NewController@add');
+    Route::get('news/create', 'Admin\NewController@add')->middleware('auth');
 });
 
 ///課題４
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
