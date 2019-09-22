0013630f55da44213b80696d3e6cee288a533c13
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-09-22 08:58:42 +0000

    9/22

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -30,13 +30,32 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
     
- public function edit()
- {
- return view('admin.profile.edit');
- }
- public function update()
- {
- return redirect('admin/profile/edit');
- }
+    public function edit(Request $request)
+    {
+     //validate
+     $this->validate($request, Profile::$rules);
+     
+      $profile = new Profile;
+      $form = $request->all();
+    // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+    
+     return view('admin.profile.edit');
+     }
+    public function update()
+    {
+        
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+    return redirect('admin/profile');
+    }
     
 }
